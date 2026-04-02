762ee7a291bb788ee726e68235cfde965fc6c6c7
Markeljan Sokoli <Markeljan19@gmail.com>
2026-04-02 05:43:14 -0400

    fix portable fzf color options

--- config/fish/conf.d/10-fzf.fish
+++ config/fish/conf.d/10-fzf.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     if type -q fzf
-        set -l fzf_color '--color=fg:-1,fg+:#d0d0d0,bg:-1,bg+:#262626,hl:#5f87af,hl+:#5fd7ff,info:#afaf87,marker:#87ff00,prompt:#00d7ff,spinner:#af5fff,pointer:#af5fff,header:#87afaf,border:#262626,label:#aeaeae,query:#d9d9d9'
+        set -l fzf_color '--color=fg:-1,fg+:#d0d0d0,bg:-1,bg+:#262626,hl:#5f87af,hl+:#5fd7ff,info:#afaf87,marker:#87ff00,prompt:#00d7ff,spinner:#af5fff,pointer:#af5fff,header:#87afaf,border:#262626,query:#d9d9d9'
         set -l fzf_layout '--border=rounded --prompt="> " --marker="*" --pointer=">" --separator="-" --scrollbar="|" --info=right --height=100% --preview-window=right:50%:border-rounded'
         set -gx FZF_DEFAULT_OPTS "$fzf_color $fzf_layout --ansi --bind 'ctrl-h:change-preview-window(hidden|)' --preview '$FZF_PREVIEW_SCRIPT {}'"
 
